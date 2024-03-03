85f829c5f79651e6ce5402aeba6829387fcfc388
elen_usepyan <elen.usepyan@gmail.com>
2024-03-04 00:07:47 +0300

    homeworke

--- matrix.py
+++ matrix.py
@@ -1,3 +1,4 @@
+#task 1
 import random
 def generator_random_matrix(row, col):
     a = [[random.randint(1, 100) for j in range(col)] for i in range(row)]
@@ -5,4 +6,28 @@ def generator_random_matrix(row, col):
 
 m = int(input())
 n = int(input())
-print(generator_random_matrix(m, n))
+matrix = generator_random_matrix(m, n)
+print(matrix)
+
+#task 2
+def get_column_sum(matrix, index):
+    summa = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if j == index - 1:
+                summa += matrix[i][j]
+    return summa
+
+index_col = int(input())
+print(get_column_sum(matrix, index_col))
+
+#task 3
+def get_row_average(matrix, index):
+    s = sum(matrix[index-1])
+    average = s / len(matrix)
+    return average
+
+index_row = int(input())
+print(get_row_average(matrix, index_row))
+
+
